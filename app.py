7a46cbf5ff4ca74102b2d5214bceff4542d47b6d
Tyson-Schmall <tyson.schmall@gmail.com>
2020-05-23 12:55:08 -0600

    Further built out database tables. Schemas are not done, the third table also needs more work done on it, progress is coming along though.

--- app.py
+++ app.py
@@ -34,13 +34,33 @@ CORS(app)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
-class Admin(db.Model):
+# Below the tables are being built. These tables are structured to withhold and store certain types of data
+# in certain combinations, to create certain behaviours and reactions when users perform specific actions.
+
+# The first table, 'Owner', this is for the owner of a shop. This individual will be the owner of the shop, the one who employs
+# everyone else. We need to be able to store their information as a user, so the database acquires this information.
+# First&Last name, email, password, these are all crucial identifying factors that should be unique.
+
+# The 'king' and 'children' variables, are for different purposes. The 'king' variable, is a Boolean data type. This means 'true' or 'false'.
+# With 'king' in place, we are able to perform checks when certain users want to perform certain actions. For example, if the Owner needs to
+# remove an employee who recently vacated their position, the Owner should be able to do that. Right? But what ensures that an 'Artist' doesn't
+# go and try to do the same thing? That's what 'king' is for. 'King' will be unique to the 'Owner' class, and the only way this system will
+# allow the removal of an artist, is if the 'king' value is set to 'True'. We will build the rest of that functionality out later, but for
+# now, this is where we begin creating the 'king' functionality we will seek later on.
+
+class Owner(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   first_name = db.Column(db.String(16), nullable=False)
   last_name = db.Column(db.String(16), nullable=False)
   email = db.Column(db.String(48), nullable=False)
   password = db.Column(db.String(32), nullable=False)
   king = db.Column(db.Boolean)
+  children = db.relationship("Artist", backref="owner")
+
+  # Below, the __init__ is us stating that for each time we 'instantiate' this class into a variable, we want the 'instance' of this class to be
+  # unique to whatever variable we have instantiated the class into. So if a shop has two Owner's, each one would be able to create their own 
+  # user information, because each owner would be creating their own 'instance' of a user. Tyson would create his own instance with his own user 
+  # credentials, and Joe would create his own instance with his user credentials. They would be totally individual of each other.
 
   def __init__(self, first_name, last_name, email, password, king):
     self.first_name = first_name
@@ -48,3 +68,39 @@ class Admin(db.Model):
     self.email = email
     self.password = password
     self.king = king
+
+
+# Below we have the Artist class. An "Artist" can only be created by an "Owner". The Artist will need certain information put in by an "Owner"
+# in order to get up and running initially. 
+
+class Artist(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  first_name = db.Column(db.String(16), nullable=False)
+  last_name = db.Column(db.String(16), nullable=False)
+  email = db.Column(db.String(48), nullable=False)
+  password = db.Column(db.String(32), nullable=False)
+  auth = db.Column(db.Boolean)
+  owner_id = db.Column(db.Integer, db.ForeignKey("owner.id"), nullable=False)
+  children = db.relationship("ArtistContent", backref="artist")
+
+  # Artist's will have a children relationship with the "ArtistContent" table. This will ensure that Artist's are allowed to udpate aspects
+  # of their online portfolio/profile page.
+
+  def __init__(self, first_name, last_name, email, password, auth):
+    self.first_name = first_name
+    self.last_name = last_name
+    self.email = email
+    self.password = password
+    self.auth = auth
+
+# Below table is not finished out yet, I had to put this on hold temporarily. Will put in further work and description of said work when 
+# further time is available. Thanks!
+
+class ArtistContent(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  cover_image = db.Column(db.String(500), nullable=False)
+  artist_name = db.Column(db.String(64), nullable=False)
+  bio = db.Column(db.String(666), nullable=False)
+
+
+
